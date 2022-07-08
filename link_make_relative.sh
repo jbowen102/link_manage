4a45d4fb2c3a7fe7d4ef48121fc3100999d0a434
Jonathan <ew15dro6k216@opayq.net>
2022-07-08 09:18:30 -0400

    Create link_make_relative.sh to convert a symlink to relative only.

---
+++ link_make_relative.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]];
+then
+  echo "Expected one argument - symlink to convert to relative." >&2
+  exit 2
+fi
+
+if [[ -e "${1}" ]]; then
+  # Not allowed to be broken link because needed calls to readlink and realpath will fail.
+	:
+elif [[ -h "${1}" ]]; then
+  echo "Link broken. Replace link target and retry." >&2
+  exit 2
+else
+  echo "Cannot find link path." >&2
+  exit 2
+fi
+
+LINK_PATH="$(realpath $(dirname ${1}))/$(basename ${1})"
+LINK_TARGET_REALPATH="$(realpath ${1})"
+
+# Temporarily switch working directory to script location to call other script
+SCRIPT_DIR="$(realpath "$(dirname "${0}")")"
+# https://code-maven.com/bash-absolute-path
+cd "${SCRIPT_DIR}"
+./link_replace -r "${LINK_PATH}" "${LINK_TARGET_REALPATH}"
+REPLACE_LINK_RETURN=$?
+
+cd - > /dev/null # suppress outputs
+
+if [[ ${REPLACE_LINK_RETURN} == 0 ]]; then
+	printf "SUCCESS\n"
+else
+  printf "FAIL: Call to link_replace unsuccessful.\n"
+	exit 1
+fi
